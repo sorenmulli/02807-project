705b449a166adbd3a9d10323fae834a310738a2a
Anders <anders-10@live.dk>
2022-11-15 14:59:58 +0100

    clean tweets too

--- dkpol/data_cleanup.py
+++ dkpol/data_cleanup.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 import pandas as pd
 
 
-def clean_data(df):
-    df.loc[df.Handle.duplicated(), ('Handle', 'Bio')] = "none"
-
-
 if __name__ == "__main__":
-    ids = pd.read_csv("data/candidates_with_id.csv")
-    clean_data(ids)
-    ids.to_csv("data/candidates_with_id_clean_final.csv")
+    handle_data = pd.read_csv("data/candidates_with_id.csv", index_col=0)
+    handle_data.loc[handle_data.Handle.duplicated(), ('Handle', 'Bio')] = "none"
+    handle_data.to_csv("data/candidates_with_id_clean_final.csv")
+
+    tweets = pd.read_csv("data/tweets.csv", index_col=0)
+    tweets = tweets.drop_duplicates(subset='tweetID', keep="first")
+    tweets.to_csv("data/tweets_clean_final.csv", index=False)
